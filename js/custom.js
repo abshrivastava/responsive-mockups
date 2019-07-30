622245dd84fb5798cf3e0e15728bb0bc79c543ec
Abhishek Shrivastava <Abhishek.Shrivastava@rsystems.com>
2019-07-30 17:09:46 +0530

    image file updated

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,10 @@
 $(document).ready(function () {
     var window_height = window.innerHeight;
     var headerHeight = $("header").innerHeight(); //header height
+    // window.location.href = "http://www.google.com";
+    //window.location.replace("http://www.google.com");
+    //document.getElementById('desktop-frame').innerHTML('http://www.google.com');
+    
     $("#content_wrapper").css("padding-top", headerHeight);
     $("#left-sidebar").css("height", window_height);
     $("#content_wrapper").css("height", window_height);
